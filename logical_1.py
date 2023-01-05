903cc3c24a6246e4480b2a29699e9d716225d73b
sushma3232 <121096745+sushma3232@users.noreply.github.com>
2023-01-05 15:04:45 +0530

    Add files via upload

---
+++ logical_1.py
@@ -0,0 +1,28 @@
+# a=9.6
+# b="91"
+# c=int(b)
+# # print(c)
+# d=int(a)
+# # print(d)
+# p=d+c
+# print(p,",",c,",",d)
+
+
+
+a="heena"
+b=10.5
+b=2.5
+c="lucky"
+d="2.8"
+# o/p:heena10.5lucky22
+
+
+
+a="indhu"
+b=5
+print(a,b)
+a="heena"
+b=10.5
+b=2.5
+c="lucky"
+d="2.8"
